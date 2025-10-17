13c9c39c6516966cf8aa089006ee2a465d9edc01
Rinku Dhurua <rinkudhurua258@gmail.com>
2025-10-17 16:57:06 +0530

    server-webhook handled

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import { inngest, functions } from "./inngest/index.js"
+import {serve} from 'inngest/express'
 dotenv.config();
 
 const app=express();
+await connectDB();
+
 app.use(express.json());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use(cors());
+
 app.get("/",(req,res)=>{
      res.send("server is running");
 })
 
 const PORT=process.env.PORT ||4000;
 app.listen(PORT,()=>{
-     console.log("listening to PORT"+PORT);
+     console.log("listening to PORT "+PORT);
 })
